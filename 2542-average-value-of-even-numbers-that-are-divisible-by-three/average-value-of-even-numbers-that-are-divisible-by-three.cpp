b02722b5d8c6323213ec209b58521a1b7ade44a0
Ishu_singh_05 <singhishusingh333@gmail.com>
2026-06-11 23:27:42 +0530

    Time: 0 ms (100.00%) | Memory: 17.3 MB (16.27%) - LeetSync

---
+++ 2542-average-value-of-even-numbers-that-are-divisible-by-three/average-value-of-even-numbers-that-are-divisible-by-three.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int averageValue(vector<int>& nums) {
+         int sum=0;
+         int count=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]%3==0&&nums[i]%2==0){
+               sum=sum+nums[i];
+                count++;
+            }
+        }
+        if(count==0){
+            return 0;
+        }
+        int s=sum/count;
+        return s;
+    }
+};
