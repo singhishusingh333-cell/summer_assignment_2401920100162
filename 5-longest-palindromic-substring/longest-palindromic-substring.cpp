3d8267b3bb650b783552d43519d54fa1417ed027
Ishu_singh_05 <singhishusingh333@gmail.com>
2026-06-19 20:53:40 +0530

    Time: 851 ms (8.22%) | Memory: 9.2 MB (92.50%) - LeetSync

---
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool solve(string &s,int i,int j){
+        if(i>=j){
+            return true;
+        }
+        else if(s[i]==s[j]){
+            return solve(s,i+1,j-1);
+        }
+        else {
+            return false;
+        }
+    }
+    string longestPalindrome(string s) {
+        int n=s.length();
+         int maxlen=INT_MIN;
+         int sp=0;
+         for(int i=0;i<n;i++){
+              for(int j=i;j<n;j++){
+                if(solve(s,i,j)==true){
+                    if(j-i+1>maxlen){
+                        maxlen=j-i+1;
+                        sp=i;
+                    }
+                }
+              }
+        }
+        return s.substr(sp,maxlen);
+    }
+};
