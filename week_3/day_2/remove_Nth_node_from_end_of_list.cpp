1d1cf01cae9fbb01082f4d3965e8c2853df8e134
Ishu_singh_05 <singhishusingh333@gmail.com>
2026-06-23 19:44:40 +0530

    Add files via upload

---
+++ week_3/day_2/remove_Nth_node_from_end_of_list.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+       ListNode*slow=head;
+       ListNode*fast=head;
+       for(int i=0;i<n;i++){
+        fast=fast->next;
+       }
+       if(fast==NULL){
+        return head->next;
+       }
+       while(fast->next!=NULL){
+        fast=fast->next;
+        slow=slow->next;
+       }
+       slow->next=slow->next->next;
+       return head;
+    }
+};
