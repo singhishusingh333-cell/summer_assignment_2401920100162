c5367ce757c138fe62f25092c53cf5364b71a15b
Ishu_singh_05 <singhishusingh333@gmail.com>
2026-07-09 23:39:17 +0530

    Time: 1 ms (87.17%) | Memory: 15.5 MB (65.84%) - LeetSync

---
+++ 506-relative-ranks/relative-ranks.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<string> findRelativeRanks(vector<int>& score) {
+        int n=score.size();
+        vector<pair<int ,int>>arr;
+        for(int i=0;i<n;i++){
+            arr.push_back({score[i],i});
+        }
+        sort(arr.begin(),arr.end(),greater<pair<int,int>>());
+        vector<string>ans(n);
+        for(int i=0;i<n;i++){
+            if(i==0){
+                ans[arr[i].second]="Gold Medal";
+            }
+            else if(i==1){
+                 ans[arr[i].second]="Silver Medal";
+            }
+            else if(i==2){
+                 ans[arr[i].second]="Bronze Medal";
+            }
+            else{
+                 ans[arr[i].second]=to_string(i+1);
+            }
+        }
+        return ans;
+    }
+};
